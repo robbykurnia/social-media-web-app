8e637f171988cfa74d200f54537bb440e83771e2
Robby <roby.kurniaesaputra@gmail.com>
2019-12-09 08:52:17 +0700

    Add Navbar and Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,31 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+import "./App.css";
+import NavBar from "./components/navBar";
+import NotFound from "./components/notFound";
+import Home from "./components/home";
+import Posts from "./components/posts";
+import About from "./components/about";
+import Other from "./components/other";
 
 class App extends Component {
-  // state = {  }
-  render() { 
-    return ( <h1>Hello World</h1> );
+  render() {
+    return (
+      <div>
+        <NavBar />
+        <div className="container">
+          <Switch>
+            <Route path="/posts" exact component={Posts} />
+            <Route path="/other" exact component={Other} />
+            <Route path="/about" exact component={About} />
+            <Route path="/not-found" component={NotFound} />
+            <Route path="/" exact component={Home} />
+            <Redirect to="/not-found" />
+          </Switch>
+        </div>
+      </div>
+    );
   }
 }
- 
+
 export default App;
---
+++ frontend/src/components/navBar.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+
+const NavBar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <Link className="navbar-brand" to="/">
+        E-Talk
+      </Link>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/">
+              Home
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/posts">
+              Posts
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/other">
+              Other
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/about">
+              About
+            </NavLink>
+          </li>
+        </ul>
+        <form className="form-inline my-2 my-lg-0">
+          <input
+            className="form-control mr-sm-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+          />
+          <button
+            className="btn btn-outline-success my-2 my-sm-0"
+            type="submit"
+          >
+            Search
+          </button>
+        </form>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
